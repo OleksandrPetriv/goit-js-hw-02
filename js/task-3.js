15daddef25259fc8c4f59b8c9de1b7c8625b53ae
Oleksandr Petriv <oleksandr.petriv62@gmail.com>
2026-07-05 18:04:37 +0200

    Home-work

--- js/task-3.js
+++ js/task-3.js
@@ -9,6 +9,7 @@ function checkForSpam(message) {
   );
 }
 
+
 console.log(checkForSpam("Latest technology news")); // false
 console.log(checkForSpam("JavaScript weekly newsletter")); // false
 console.log(checkForSpam("Get best sale offers now!")); // true
